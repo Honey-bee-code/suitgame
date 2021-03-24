526fb6d5053ef7e5ad9def17c60d7cfd6c962327
Honey-bee-code <hanifinida@gmail.com>
2021-03-24 11:14:52 +0800

    uppercase hasil

--- gamesuit.js
+++ gamesuit.js
@@ -8,27 +8,31 @@ while (lagi) {
   var win = "Horee, KAMU MENANG!";
   var lose = "Kasihan, KAMU KALAH!";
   var p = prompt("Silahkan ketik: gajah/semut/orang");
-  if (c < 0.34) {
-    c = g;
-  } else if (c > 0.34 && c < 0.67) {
-    c = s;
-  } else if (c > 0.67) {
-    c = o;
-  }
+  if (p) {
+    if (c < 0.34) {
+      c = g;
+    } else if (c > 0.34 && c < 0.67) {
+      c = s;
+    } else if (c > 0.67) {
+      c = o;
+    }
 
-  if (p == c) {
-    h = "SERI!";
-  } else if (p == g) {
-    h = c == o ? win : lose;
-  } else if (p == s) {
-    h = c == g ? win : lose;
-  } else if (p == o) {
-    h = c == s ? win : lose;
+    if (p == c) {
+      h = "SERI!";
+    } else if (p == g) {
+      h = c == o ? win : lose;
+    } else if (p == s) {
+      h = c == g ? win : lose;
+    } else if (p == o) {
+      h = c == s ? win : lose;
+    } else {
+      alert("Salah input, silahkan ketik salah satu di antara 3 pilihan:\n- gajah\n- semut\n- orang");
+    }
+
+    alert("Kamu memilih " + p.toUpperCase() + "\nComputer memilih " + c.toUpperCase() + "\n\nHasilnya : " + h);
+    lagi = confirm("Ingin bermain lagi?");
   } else {
-    alert("Salah input, silahkan ketik salah satu di antara 3 pilihan:\ngajah\nsemut\norang");
+    alert("Silahkan bermain dulu, bentar aja kok!");
   }
-
-  alert("Kamu memilih " + p + "\nComputer memilih " + c + "\nHasilnya : " + h);
-  lagi = confirm("Ingin bermain lagi?");
 }
 alert("Terima kasih sudah bermain!");
